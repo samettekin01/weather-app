9333d74154d9c860761b3cc62d733aac69a91d6e
samettekin01 <samet.tekin.01@gmail.com>
2023-10-20 20:42:39 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import cities from "./data/cities.json"
+import { Listbox } from '@headlessui/react';
+import WeatherCard from './components/weatherCard';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState(cities[0]);
+  const [city, setCity] = useState("Ankara");
+  const [cityWeather, setCityWeather] = useState();
+
+  useEffect(() => {
+    setCity(list.city)
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ac9d660f2abc55c4c76f24bffb8874cf&units=metric&cnt=5`)
+      .then(p => setCityWeather(p.data));
+  }, [city, list]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='relative w-64 bg-white rounded-xl mt-5'>
+          <Listbox value={list} onChange={setList}>
+            <Listbox.Button className="listbox-button">{list.city}</Listbox.Button>
+            <Listbox.Options className="listbox-options">
+              {cities && cities.map((data, i) =>
+                <Listbox.Option
+                  className={({ active }) =>
+                    `p-1 ${active ? ` cursor-pointer bg-sky-900 text-white rounded-lg outline outline-1 outline-sky-300` : `bg-sky-50`}`
+                  }
+                  key={i}
+                  value={data}
+                >
+                  {data.city}
+                </Listbox.Option>
+              )}
+            </Listbox.Options>
+          </Listbox>
+        </div>
+        <WeatherCard data={cityWeather} city={city} />
+      </div>
     </div>
   );
 }
---
+++ src/components/weatherCard.jsx
@@ -0,0 +1,49 @@
+import "./weather-card.css"
+
+function WeatherCard({ data, city }) {
+
+    const setDate = (date) => {
+        const d = new Date(date);
+        const history = `${d.getDay()}.${d.getMonth() + 1}.${d.getFullYear()}`
+        const hours = `${d.getHours()}:${d.getMinutes()}0`
+        return { history, hours }
+    }
+    
+    return (
+        <div className="weathercard">
+            {
+                data && data.list.map((d, i) => (
+                    <div key={i} className={`city-weather-container `} >
+                        <div className='city-name text-center text-white text-2xl'>{city},{data.city.country}</div>
+                        <div className='date text-center text-white'>{`${setDate(d.dt_txt).history}`}</div>
+                        <div className='date text-center text-white'>{`${setDate(d.dt_txt).hours}`}</div>
+                        <div className='weather-img flex flex-col justify-center'>
+                            <div
+                                className='w-9/10'
+                                style={{
+                                    width: "100%",
+                                    height: "80px",
+                                    backgroundPosition: "center",
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundImage: `url(https://openweathermap.org/img/wn/${d.weather[0].icon}@2x.png)`
+                                }}
+                                alt={city}
+                            />
+                        </div>
+                        <div className='text-center text-white text-lg'>Clear</div>
+                        <div className='city-weather-temp text-center text-white text-lg'>
+                            <div className='p-5'>Temp: {d.main.temp}°</div>
+                            <div className='flex flex-row justify-center'>
+                                <span className='text-sm'>Max:<br /> {d.main.temp_max}<small>°</small></span>
+                                <div className='border-solid border-orange-500 rounded-sm border-2 opacity-40 mr-5 ml-5'></div>
+                                <span className='text-sm'> Min:<br />{d.main.temp_min}°</span>
+                            </div>
+                        </div>
+                    </div>
+
+                ))};
+        </div>
+    )
+}
+
+export default WeatherCard;
